c0d1c0a53c9af87ffc3d6408e2354cd8b4187716
Zer0553 <124603918+Zer0553@users.noreply.github.com>
2023-02-07 10:46:52 +0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -5,37 +5,53 @@ num = ['1','2','3','4','5','6','7','8','9','0']
 lines = []
 numbers = []
 
+
+def words(n):
+    f = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять',
+         6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+    return f.get(n)
+
+
 with open('NaL.txt') as f:
     file = f.read()
     for i in file.split():
         lines.append(i)
 
 for line in lines:
+    h = 0
     for i in range(len(line)):
-        if line[i] in start_num:
-            number = line[i]
-            even = 0
-            while even < 2:
-                try:
-                    if line[i + 1] in num:
-                        if int(line[i + 1]) % 2 == 0:
-                            even += 1
-                            if even == 2:
-                                number += line[i + 1]
-                                break
-                            else:
-                                number += line[i + 1]
-                    if line[i + 1] not in num:
+        if h > 0:
+            h -= 1
+            break
+        else:
+            if line[i] in start_num:
+                number = line[i]
+                h = 1
+                while True:
+                    try:
+                        if line[i + h] in num:
+                            number += line[i + h]
+                        if line[i + h] not in num:
+                            break
+                        h += 1
+                    except IndexError:
                         break
-                    i += 1
-                except IndexError:
-                    break
-            if even in [1, 2]:
                 numbers.append(number)
+                
+nnumbers = []
 
 for i in numbers:
-    output = str(i) + ' - '
+    even = 0
     for j in i:
         if int(j) % 2 == 0:
-            output += j + ' '
+            even += 1
+    if even == 2:
+        nnumbers.append(i)
+
+
+for i in nnumbers:
+    output = str(i) + ' - ' + words(int(i[0]))
+    for j in i:
+        if int(j) % 2 == 0:
+            output += ' ' + words(int(j))
     print(output)
